394c378b403a7cae19c64747a33971e3a731f019
David Reale <dagn391@gmail.com>
2021-02-12 12:55:03 -0500

    Added Procfile

--- src/test/java/com/example/servingwebcontent/Lab4ApplicationTests.java
+++ src/test/java/com/example/servingwebcontent/Lab4ApplicationTests.java
@@ -1,16 +1,21 @@
 package com.example.servingwebcontent;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class Lab4ApplicationTests {
 
-	/*
+	@Autowired
+	private RestController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
-	*/
 
 
 }
